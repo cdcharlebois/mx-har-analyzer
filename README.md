b84dc50fb7aeb0a98c6ffd2ec06e95bfad7707c3
Conner Charlebois <cdcharlebois@gmail.com>
2023-06-29 19:16:25 -0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+At a high level, this is what we're looking for:
+
+a refresh_class that causes a bunch of other retrieves
+- a refresh_class followed by one/more retrieves where the CurrentObject's guid matches the class
+- match the first N digits of the guid?
+
+map queryId to xpath or other retrieve (type?)
---
+++ index.js
@@ -0,0 +1,70 @@
+const fs = require('fs');
+// const file = 
+
+// let har;
+// fs.readFile('file', 'utf8', function (err, data) {
+//     if (err) throw err;
+//     har = JSON.parse(data);
+// });
+
+const path = process.argv[2];
+const har = require(path);
+const requests = har.log.entries.filter(entry => {
+    return entry.request.method === "POST" && /xas\/$/.test(entry.request.url)
+})
+    .map(entry => ({
+        request: entry.request,
+        response: entry.response
+    }));
+console.log(requests.length);
+let refreshes = [];
+const mxReqLog = requests.map((req, index) => {
+    let ret = {};
+    
+    const { text } = req.request.postData;
+    const { action, params } = JSON.parse(text);
+    ret.request = {
+        action, params, queryId: params.queryId,
+    }
+    const responseText = req.response.content.text;
+    const { changes, commits, instructions, objects } = JSON.parse(responseText)
+    const refreshInClient = instructions && instructions.find(instr => {
+        return instr.type == "refresh_class"
+    })
+    if (refreshInClient) {
+        refreshes.push({
+            id: index,
+            entities: refreshInClient.args.classnames,
+            guids: instructions.find(instr => {
+                return instr.type == "refresh_object_list"
+            }).args.ObjectIds
+        })
+    }
+    ret.response = {
+        changes, commits, instructions,
+        objects: objects.map(fullObj => ({ guid: fullObj.guid, objectType: fullObj.objectType }))
+    }
+    let matchingRefresh;
+    if (action === "retrieve" && params.params.CurrentObject){
+        const reqContextGuid = params.params.CurrentObject.guid;
+        matchingRefresh = refreshes.find(refresh => {
+            return refresh.guids.find(g => {
+                return g.substring(0,5) === reqContextGuid.substring(0,5)
+            })
+        })
+    }
+    
+    ret.analysis = {
+        id: index,
+        action: action,
+        queryId: params.queryId,
+        source: 'TBD',
+        trigger: matchingRefresh ? matchingRefresh.id : undefined
+
+
+    }
+    return ret;
+})
+console.log(refreshes);
+// console.log(mxReqLog);
+fs.writeFileSync('./out.json', JSON.stringify(mxReqLog));
