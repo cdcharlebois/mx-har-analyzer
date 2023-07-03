6b41131f1d4db3e7c3934565b8b13be9e9c77839
Conner Charlebois <cdcharlebois@gmail.com>
2023-07-03 13:58:24 -0400

    build data structure of given page

--- modelsdk.js
+++ modelsdk.js
@@ -2,6 +2,37 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.main = void 0;
 const mendixplatformsdk_1 = require("mendixplatformsdk");
+function isDataElement(structureTypeName) {
+    return ["Pages$DataView", "Pages$ListView"].indexOf(structureTypeName) > -1;
+}
+function getParentDataElement(structure) {
+    // @ts-ignore
+    const { name, $Type, $ID } = structure.toJSON();
+    const ret = { name, $Type, $ID };
+    if (isDataElement(structure.structureTypeName)) {
+        return ret;
+    }
+    else if (structure.structureTypeName === "Pages$Page") {
+        return ret;
+    }
+    else {
+        return getParentDataElement(structure.container);
+    }
+}
+function buildPageTreeNodesFrom(pde, all) {
+    const children = all.filter((dataElement) => {
+        return dataElement.dataParent.$ID === pde.$ID;
+    });
+    if (children.length === 0) {
+        return pde;
+    }
+    else {
+        return {
+            ...pde,
+            children: children.map(child => buildPageTreeNodesFrom(child, all))
+        };
+    }
+}
 async function main(token, appid) {
     // console.log("hello there");
     (0, mendixplatformsdk_1.setPlatformConfig)({
@@ -12,17 +43,44 @@ async function main(token, appid) {
     const workingCopy = await app.createTemporaryWorkingCopy("Release2.0");
     const model = await workingCopy.openModel();
     const pages = await model.allPages();
-    const pageTree = [];
-    // console.log(pages);
-    pages[10].asLoaded().traverse(structure => {
-        // if (structure.structureTypeName = "Pages$Page"){
-        // structure.traverse(s2 => {
-        pageTree.push(structure.structureTypeName);
-        // })
-        // }
+    const lpage = await pages[0].load();
+    // console.log(lpage.toJSON())
+    // console.log(`title: ${lpage.title}`);
+    const pageTree = {};
+    const pageDataElements = [];
+    lpage.traverse(structure => {
+        if (isDataElement(structure.structureTypeName)) {
+            // console.log(`id: ${structure.id}, unit: ${structure.unit}`);
+            // @ts-ignore
+            const { name, $Type, $ID } = structure.toJSON();
+            // const unit = structure.unit;
+            const ret = {
+                name, $Type, $ID, dataParent: getParentDataElement(structure.container)
+            };
+            // console.log(ret)
+            pageDataElements.push(ret);
+        }
     });
-    console.log(pages[10].name);
-    console.log(pageTree);
+    const roots = pageDataElements.filter((dataElement) => {
+        return dataElement.dataParent.$Type === "Pages$Page";
+    });
+    roots.forEach((root) => {
+        const leaf = buildPageTreeNodesFrom(root, pageDataElements);
+        // @ts-ignore
+        pageTree[root.$ID] = leaf;
+    });
+    console.dir(pageTree, { depth: null });
+    // const pageTree: string[] = [];
+    // console.log(pages);
+    // pages[10].asLoaded().traverse(structure => {
+    //     // if (structure.structureTypeName = "Pages$Page"){
+    //         // structure.traverse(s2 => {
+    //             pageTree.push(structure.structureTypeName);
+    //         // })
+    //     // }
+    // });
+    // console.log(pages[10].name);
+    // console.log(pageTree);
     model.closeConnection();
 }
 exports.main = main;
--- modelsdk.ts
+++ modelsdk.ts
@@ -1,6 +1,50 @@
 // modelsdk.ts
-import {domainmodels} from "mendixmodelsdk";
+import {IStructure, domainmodels} from "mendixmodelsdk";
 import {MendixPlatformClient, setPlatformConfig} from "mendixplatformsdk";
+// import {util}
+// const util = require("util");
+// import {} from "menx"
+
+interface PageDataElement {
+    name: string,
+    $Type: string,
+    $ID: string,
+    dataParent?: PageDataElement
+}
+
+function isDataElement(structureTypeName: string): boolean {
+    return ["Pages$DataView", "Pages$ListView"].indexOf(structureTypeName) > -1;
+}
+
+function getParentDataElement(structure: IStructure): PageDataElement {
+    // @ts-ignore
+    const { name, $Type, $ID } = structure.toJSON();
+    const ret = {name, $Type, $ID};
+    if (isDataElement(structure.structureTypeName)) {
+        return ret;
+    }
+    else if (structure.structureTypeName === "Pages$Page"){
+        return ret;
+    }
+    else {
+        return getParentDataElement(structure.container as IStructure);
+    }
+}
+
+function buildPageTreeNodesFrom(pde: PageDataElement, all: PageDataElement[]): {} {
+    const children = all.filter((dataElement: any) => {
+        return dataElement.dataParent.$ID === pde.$ID
+    })
+    if (children.length === 0){
+        return pde
+    } else {
+        return {
+            ...pde,
+            children: children.map(child => buildPageTreeNodesFrom(child, all))
+        }
+    }
+    
+}
 
 export async function main(token: string, appid: string) {
     // console.log("hello there");
@@ -15,7 +59,35 @@ export async function main(token: string, appid: string) {
     const model = await workingCopy.openModel();
     
     const pages = await model.allPages()
-    const pageTree: string[] = [];
+    const lpage = await pages[0].load();
+    // console.log(lpage.toJSON())
+    // console.log(`title: ${lpage.title}`);
+    const pageTree = {};
+    const pageDataElements: PageDataElement[] = [];
+    lpage.traverse(structure => {
+        if (isDataElement(structure.structureTypeName)){
+            // console.log(`id: ${structure.id}, unit: ${structure.unit}`);
+            // @ts-ignore
+            const { name, $Type, $ID } = structure.toJSON();
+            // const unit = structure.unit;
+            const ret = {
+                name, $Type, $ID, dataParent: getParentDataElement(structure.container as IStructure)
+            }
+            // console.log(ret)
+            pageDataElements.push(ret);
+        }
+    })
+
+    const roots = pageDataElements.filter((dataElement: any) => {
+        return dataElement.dataParent.$Type === "Pages$Page";
+    })
+    roots.forEach((root: PageDataElement) => {
+        const leaf = buildPageTreeNodesFrom(root, pageDataElements);
+        // @ts-ignore
+        pageTree[root.$ID] = leaf;
+    })
+    console.dir(pageTree, {depth: null});
+    // const pageTree: string[] = [];
     // console.log(pages);
     // pages[10].asLoaded().traverse(structure => {
     //     // if (structure.structureTypeName = "Pages$Page"){
